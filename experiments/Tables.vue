20834d1ccf2cdc356fb022dbfca2fdb5e40d4f98
Farid <Farid Abizeid>
2017-11-23 01:50:05 +0200

    saving some experiments

---
+++ experiments/Tables.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="landing container">
+
+    <b-form-input class="w-50 my-2 ml-auto" v-model="filter2" placeholder="Type to Search" />
+    <b-table 
+	     :items="booksFB" 
+	     :filter="filter2"
+	     >
+      <template slot="HEAD_age" slot-scope="data">
+	<div> 
+	  <!-- <a>{{data.field.label}}</a>  -->
+	  <b-form-input size="sm" class="mr-auto mt-2 w-50" v-bind:placeholder="'Search ' + data.field.label" />
+	</div>
+      </template>
+      <template slot="HEAD_name" slot-scope="data">
+	<div> 
+	  <!-- <a>{{data.field.label}}</a>  -->
+	  <b-form-input size="sm" class="mr-auto mt-2 w-50" v-bind:placeholder="'Search ' + data.field.label" />
+	</div>
+      </template> 
+    </b-table>
+
+
+    <input v-model="search" class="form-control" placeholder="Filter users by name or age">
+      <table class="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in filteredBooks">
+          <td>{{book.name}}</td>
+          <td>{{book.age}}</td> 
+        </tr>
+	</tr>
+      </tbody>
+    </table>
+ 
+  </div>
+</template>
+
+<script>
+  var componentData = {
+  
+      booksFB: [{ name: 'John', age: 50 },
+		{ name: 'Jane', age: 22 },
+		{ name: 'Paul', age: 34 },
+		{ name: 'Kate', age: 15 },
+		{ name: 'Amanda', age: 65 },
+		{ name: 'Steve', age: 38 },
+		{ name: 'Keith', age: 21 },
+		{ name: 'Don', age: 50 },
+		{ name: 'Susan', age: 21 }],
+      search: '',
+      filter2: null
+  };
+
+
+export default {
+    name: 'Landing',
+    data () {
+	return componentData;
+    },
+    computed: {
+	filteredBooks: function () {
+	    //var self = this
+	    return this.booksFB.filter(  
+		//book => book.name.indexOf(this.search) !== -1
+		book => {
+		    var searchRegex = new RegExp(this.search, 'i')
+		    return (
+			searchRegex.test(book.name) ||
+			    searchRegex.test(book.age)
+		    )
+		})
+	}
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
